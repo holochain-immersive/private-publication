03c0c2c505f3bf74ff21e08e7318a3a2ee0618c4
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-03-12 19:50:24 +0100

    Removed unnecessary import

--- dnas/private_publication/integrity_zomes/private_publication/src/membrane.rs
+++ dnas/private_publication/integrity_zomes/private_publication/src/membrane.rs
@@ -3,8 +3,6 @@ use std::sync::Arc;
 use hdi::prelude::*;
 use membrane_proof::PrivatePublicationMembraneProof;
 
-use crate::properties::progenitor;
-
 pub fn is_membrane_proof_valid(
     for_agent: AgentPubKey,
     membrane_proof: Option<MembraneProof>,
