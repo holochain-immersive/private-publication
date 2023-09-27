d28f8738e7320c2137b42044060013a79d87180b
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-09-27 14:57:09 +0200

    Upped hdk

--- dnas/lobby/coordinator_zomes/private_publication_lobby/src/lib.rs
+++ dnas/lobby/coordinator_zomes/private_publication_lobby/src/lib.rs
@@ -1,4 +1,8 @@
+/**
+ * Add your edits to the bottom of this file
+ */
+use hdk::prelude::*;
 
-/** Don't change */
+// Don't change
 #[cfg(feature = "exercise")]
 extern crate private_publication_lobby;
